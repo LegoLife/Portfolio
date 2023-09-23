bc70f11bb53bcd72dcc3c2eb0b84827a1902ab2f
jls <jsteffen@hepn.com>
2023-09-23 11:27:50 -0400

    added remove watermark feature for webhosting site

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Enumerable from "linq";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements  OnInit{
+  ngOnInit(): void {
+    Enumerable.from(document.getElementsByTagName("img")).forEach(x=>{
+      if(x.alt ==="www.000webhost.com")
+        x.remove();
+    })
+    }
 
 }
